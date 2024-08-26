37eea5f64dfb1261ca4b4fb1029012cb56bce1f4
Yousef Meska <mrbomberboy123@gmail.com>
2024-08-26 08:18:17 +0300

    initialize project

---
+++ Angular/final-project/src/app/Components/Forms/template-deriven-form/template-deriven-form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { StaticDataService } from '../../../Services/static-data.service';
+import { IProduct } from '../../../Models/iproduct';
+import { JsonPipe } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-template-deriven-form',
+  standalone: true,
+  imports: [FormsModule , JsonPipe],
+  providers: [StaticDataService],
+  templateUrl: './template-deriven-form.component.html',
+  styleUrl: './template-deriven-form.component.css'
+})
+export class TemplateDerivenFormComponent {
+  newPrd: IProduct = {} as IProduct;
+  constructor(private staticdata: StaticDataService , private router:Router) { }
+  addProduct() {
+    this.staticdata.addPrd(this.newPrd)
+    console.log(this.newPrd);
+    this.router.navigateByUrl("/products")
+  }
+}
---
+++ Angular/final-project/src/app/Components/shopping/shopping.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { DynamicDataService } from '../../Services/dynamic-data.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-shopping',
+  standalone: true,
+  imports: [HttpClientModule],
+  providers: [DynamicDataService],
+  templateUrl: './shopping.component.html',
+  styleUrl: './shopping.component.css'
+})
+export class ShoppingComponent implements OnInit {
+  data: any[] = [];
+  constructor(private dynamicdata: DynamicDataService) { }
+
+  GetAll() {
+    this.dynamicdata.getAllData().subscribe(prds => this.data = prds)
+  }
+  ngOnInit(): void {
+    this.GetAll()
+    console.log(this.data);
+  }
+}
